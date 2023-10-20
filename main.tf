4d1066263be04371a0b9d37dace57bf50410bc6a
Akin <kuyg0002@algonquinlive.com>
2023-10-20 00:08:03 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "midgr3RG" {
+  name     = "midgr3RG"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "midgr3RG"
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+    subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}
+
+
+
